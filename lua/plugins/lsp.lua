58d9b21632cf01f9281c72be973a9c5f588cbcd3
Noah Lin <popshia@gmail.com>
2023-11-15 17:09:47 +0800

    tweaking lualine.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,9 +2,23 @@ return {
   "neovim/nvim-lspconfig",
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- disable default keymappings
     keys[#keys + 1] = { "K", false }
+    keys[#keys + 1] = { "gI", false }
     keys[#keys + 1] = { "<leader>cr", false }
+
+    -- change gk to hover
     keys[#keys + 1] = { "gk", vim.lsp.buf.hover, desc = "Hover" }
+
+    -- change gk to hover
+    keys[#keys + 1] = {
+      "gi",
+      function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end,
+      desc =
+      "Goto Implementation"
+    }
+
+    -- change gr to rename symbol
     if require("lazyvim.util").has("inc-rename.nvim") then
       keys[#keys + 1] = {
         "gr",
@@ -19,5 +33,8 @@ return {
     else
       keys[#keys + 1] = { "gr", vim.lsp.buf.rename, desc = "Rename", has = "rename" }
     end
+
+    -- add gl to open_float, but there's already a ghost text for diagnostics.
+    -- keys[#keys + 1] = { "gl", vim.diagnostic.open_float }
   end,
 }
